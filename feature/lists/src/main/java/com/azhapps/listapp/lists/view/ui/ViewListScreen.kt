469b53ab517b25d8bc40dd17153cf0409931628a
Karl Beier <karl.beier1@gmail.com>
2022-10-06 20:26:51 +1100

    wip list display ui

--- feature/lists/src/main/java/com/azhapps/listapp/lists/view/ui/ViewListScreen.kt
+++ feature/lists/src/main/java/com/azhapps/listapp/lists/view/ui/ViewListScreen.kt
@@ -4,6 +4,7 @@ package com.azhapps.listapp.lists.view.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
@@ -24,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.azhapps.listapp.common.ui.theme.ListAppTheme
 import com.azhapps.listapp.common.ui.theme.Typography
+import com.azhapps.listapp.lists.model.Category
 import com.azhapps.listapp.lists.navigation.ViewList
 import com.azhapps.listapp.lists.ui.lazyHeader
 import com.azhapps.listapp.lists.view.ViewListViewModel
@@ -31,6 +34,8 @@ import com.azhapps.listapp.lists.view.model.ListItemState
 import com.azhapps.listapp.lists.view.model.ViewListAction
 import dev.enro.annotations.ExperimentalComposableDestination
 import dev.enro.annotations.NavigationDestination
+
+private val ROW_HEIGHT = 32.dp
 
 @Composable
 @ExperimentalComposableDestination
@@ -54,11 +59,14 @@ fun ViewListContent(
     itemStates: Map<String, List<ListItemState>>,
     actor: (ViewListAction) -> Unit,
 ) {
-    Column(Modifier.fillMaxWidth()) {
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(all = ListAppTheme.defaultSpacing)
+    ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(all = ListAppTheme.defaultSpacing),
+                .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
         ) {
             Text(
@@ -73,33 +81,52 @@ fun ViewListContent(
             )
         }
 
-        LazyColumn(content = {
-            itemStates.forEach { entry ->
-                lazyHeader(entry.key)
-                entry.value.forEach { itemState ->
-                    listItem(
-                        state = itemState,
-                        actor = actor,
+        LazyColumn(
+            modifier = Modifier.padding(bottom = ListAppTheme.defaultSpacing),
+            content = {
+                itemStates.forEach { entry ->
+                    lazyHeader(entry.key)
+                    entry.value.forEach { itemState ->
+                        listItem(
+                            state = itemState,
+                            actor = actor,
+                        )
+                    }
+                    addItemCard(
+                        category = null,
+                        actor = actor
                     )
                 }
-                item {
-                    IconButton(onClick = {
-                        actor(ViewListAction.CreateItem(entry.value.firstOrNull()?.item?.category))
-                    }) {
-                        Icon(imageVector = Icons.Outlined.Add, contentDescription = "Add Item")
-                    }
+                if (itemStates.isEmpty()) {
+                    addItemCard(
+                        category = null,
+                        actor = actor
+                    )
                 }
-            }
-            if (itemStates.isEmpty()) {
-                item {
-                    IconButton(onClick = {
-                        actor(ViewListAction.CreateItem(null))
-                    }) {
-                        Icon(imageVector = Icons.Outlined.Add, contentDescription = "Add Item")
-                    }
+            })
+    }
+}
+
+private fun LazyListScope.addItemCard(
+    category: Category?,
+    actor: (ViewListAction) -> Unit,
+) {
+    item {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(ROW_HEIGHT)
+                .padding(bottom = 2.dp),
+            elevation = 2.dp,
+        ) {
+            IconButton(
+                onClick = {
+                    actor(ViewListAction.CreateItem(category))
                 }
+            ) {
+                Icon(imageVector = Icons.Outlined.Add, contentDescription = "Add Item")
             }
-        })
+        }
     }
 }
 
@@ -127,5 +154,3 @@ private fun LazyListScope.listItem(
         }
     }
 }
-
-private val ROW_HEIGHT = 16.dp
