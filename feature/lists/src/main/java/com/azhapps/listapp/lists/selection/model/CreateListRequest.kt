f98053debfd7a95caa28e91a91405a0d04ad72e0
Karl Beier <karl.beier1@gmail.com>
2022-11-09 20:51:17 +1100

    Update list creation contract

--- feature/lists/src/main/java/com/azhapps/listapp/lists/selection/model/CreateListRequest.kt
+++ feature/lists/src/main/java/com/azhapps/listapp/lists/selection/model/CreateListRequest.kt
@@ -1,18 +1,16 @@
 package com.azhapps.listapp.lists.selection.model
 
-import com.azhapps.listapp.lists.model.Category
-import com.azhapps.listapp.common.model.Group
 import com.azhapps.listapp.lists.model.InformativeList
 import com.google.gson.annotations.SerializedName
 
 data class CreateListRequest(
     val name: String,
     @SerializedName("group_id") val groupId: Int?,
-    val category: Category?,
+    @SerializedName("category_id") val categoryId: Int?,
 )
 
 fun InformativeList.toCreateRequest() = CreateListRequest(
     name = name,
     groupId = group?.id,
-    category = category,
+    categoryId = category?.id,
 )
